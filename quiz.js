09faf290f33bd5fe1f134bc5d1ad493b9c272e6f
Tomas <felicianoalmeidatomas@gmail.com>
2026-05-04 15:16:24 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -35,11 +35,60 @@ function frCdn(){
   }
 
    let res5=prompt("Quel type d’expansion est « fatigué » dans « L’enfant fatigué s’endort. » ?")
-  if (res4==="adjectif"){
+  if (res5==="adjectif"){
     alert("Bravo tu as trouvé")
     score=(score+1)
   } else{
     alert("Faux,c'était l'adjectif")
+
+  }
+
+alert("Tu as" +score +"sur 5")
+}
+
+
+function frPhraseComplexe(){
+  let score=0
+  let res=prompt("Lequel n'est pas un moyen de lier des phrases:la subordination,la coordination,la juxtaposition ou le collage")
+  if (res==="le collage"){
+    alert("Bravo tu as trouvé")
+    score=(score+1)
+  } else{
+    alert("Faux,c'était le collage")
+  }
+  let res2=prompt("Dans la phrase « Le chat noir dort mais il a peur », quelle est la liaison ?")
+  if (res2==="coordination"){
+    alert("Bravo tu as trouvé")
+    score=(score+1)
+  } else{
+    alert("Faux,c'était la coordination")
+
+  }
+
+  let res3=prompt("Dans « Le livre que je lis est passionnant puisuqe je l'ai terminé. », quelle est la liaison ?")
+  if (res3==="subordination"){
+    alert("Bravo tu as trouvé")
+    score=(score+1)
+  } else{
+    alert("Faux,c'était de la subordination")
+
+  }
+
+  let res4=prompt("Dans « La maison de mon oncle est grande,je la réduirais à une petite taille », quelle est la liaison? » ?")
+  if (res4==="juxtaposition"){
+    alert("Bravo tu as trouvé")
+    score=(score+1)
+  } else{
+    alert("Faux,c'était de la juxtaposition")
+
+  }
+
+   let res5=prompt("Je mangerais mon poulet quand il fera froid,quel est la subordonnée(recopie la)")
+  if (res5==="quand il fera froid"){
+    alert("Bravo tu as trouvé")
+    score=(score+1)
+  } else{
+    alert("Faux,c'était quand il fera froid")
 
   }
 
