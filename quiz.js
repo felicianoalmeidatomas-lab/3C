c24029ee87bf30508c7f4dc43e72d5176e621675
Tomas <felicianoalmeidatomas@gmail.com>
2026-06-09 14:08:51 +0200

    Add menu function to toggle display of Menu

--- quiz.js
+++ quiz.js
@@ -1,3 +1,20 @@
+function menu(){
+ let Menuu=document.getElementById("Menu")
+
+ if (Menuu.style.display==="none"){
+  Menuu.style.display="block"
+ } else{
+  Menuu.style.display="none"
+ }
+}
+
+
+
+
+
+
+
+
 //Expansions du nom
 
 function frCdn(){
