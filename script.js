18f57aec9fb122ecc15066848fa30588704c9178
Tomas <felicianoalmeidatomas@gmail.com>
2026-06-11 15:18:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function back(){
   let couleur=prompt('Quelle couleur veux tu pour le fond d écran?(en Anglais)')
   document.body.style.backgroundColor=couleur
 }
+
+function mode(){
+  let mode=document.getElementById("mode")
+
+  if (mode.src.includes("https://uploads.onecompiler.io/44qdjw5sd/1781182969607/mode-nuit.png")){
+    mode.src="https://uploads.onecompiler.io/44qdjw5sd/1781182972269/mode-lumiere.png"
+    document.body.style.backgroundColor="#3B3B3D"
+  }else { 
+     mode.src="https://uploads.onecompiler.io/44qdjw5sd/1781182969607/mode-nuit.png"
+     document.body.style.backgroundColor="#e2d0eab3"
+  }
+}
